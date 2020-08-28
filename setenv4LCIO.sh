0b07703dc1b395dece9fd149bb3920878a29c9cd
Sho Iwamoto <misho@login.snowmass21.ci-connect.net>
2020-08-28 11:15:42 -0500

    complete the first example with minor modifications on env

--- setenv4LCIO.sh
+++ setenv4LCIO.sh
@@ -1,11 +1,20 @@
-!/bin/sh
-
 #
 # setup environment to use right root, gcc, python and cmake for LCIO
 #
+
+# I'm so nervous that I always use no_unset, but here I'll relax it.
+unsetopt no_unset
 
 . /cvmfs/sft.cern.ch/lcg/releases/gcc/8.2.0-3fa06/x86_64-slc6/setup.sh
 export PATH=/cvmfs/ilc.desy.de/sw/x86_64_gcc82_sl6/CMake/3.15.5/bin/:$PATH
 export PATH=/cvmfs/sft.cern.ch/lcg/releases/LCG_96/Python/2.7.16/x86_64-slc6-gcc8-opt/bin:${PATH}
 export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/LCG_96/Python/2.7.16/x86_64-slc6-gcc8-opt/lib:${LD_LIBRARY_PATH}
 . /cvmfs/ilc.desy.de/sw/x86_64_gcc82_sl6/root/6.18.04/bin/thisroot.sh
+
+setopt no_unset
+
+# Misho's specific ENV setup
+#
+# -- LCIO
+export LCIO=~/softwares/lcio
+export LD_LIBRARY_PATH=$LCIO/lib:$LD_LIBRARY_PATH
